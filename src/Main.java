9085d625f03f274efb430da1c4270cb37aa4dfca
juliusbacosa <juliusbacosa@gmail.com>
2016-01-17 17:25:38 +0800

    - disableMenu

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
@@ -24,18 +25,35 @@ public class Main extends Application{
         window.setTitle("Menu");
 
         //file menu
-        Menu fileMenu = new Menu("File");
+        Menu fileMenu = new Menu("_File");
 
         //Menu Items
-        fileMenu.getItems().add(new MenuItem("New Project..."));
-        fileMenu.getItems().add(new MenuItem("New Module..."));
-        fileMenu.getItems().add(new MenuItem("Import Project..."));
+        MenuItem newFile = new MenuItem("New...");
+        newFile.setOnAction(e->System.out.println("Create a new file..."));
+        fileMenu.getItems().add(newFile);
+
+        fileMenu.getItems().add(new MenuItem("Open..."));
+        fileMenu.getItems().add(new MenuItem("Save..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Settings..."));
+        fileMenu.getItems().add(new SeparatorMenuItem());
         fileMenu.getItems().add(new MenuItem("Exit..."));
+
+
+        //Edit Menu
+        Menu editMenu = new Menu("_Edit");
+        editMenu.getItems().add(new MenuItem("Cut"));
+        editMenu.getItems().add(new MenuItem("Copy"));
+
+
+        MenuItem paste = new MenuItem("Paste");
+        paste.setOnAction(e->System.out.println("Pasting..."));
+        paste.setDisable(true);
+        editMenu.getItems().add(paste);
 
         //Main menu bar
         MenuBar menuBar = new MenuBar();
-        menuBar.getMenus().addAll(fileMenu);
+        menuBar.getMenus().addAll(fileMenu,editMenu);
 
         layout = new BorderPane();
         layout.setTop(menuBar);
